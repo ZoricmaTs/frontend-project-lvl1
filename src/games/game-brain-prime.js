c2a3fdbd465c11f670c1a3eb2a1b564f7dffb5b1
ZoricmaTs <tszzzch@gmail.com>
2021-05-29 21:02:02 +0700

    changed prime-game

--- src/games/game-brain-prime.js
+++ src/games/game-brain-prime.js
@@ -4,13 +4,17 @@ import game from '../index.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
 
-  return num > 1;
+  return true;
 };
 
 const createBrainPrime = () => {
